1936b5943698d81549640549397703a5543f66c7
gulsum <gulsumbastan@gmail.com>
2023-03-18 20:48:01 +0300

    .

--- Business/Abstract/IAuthService.cs
+++ Business/Abstract/IAuthService.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace Business.Abstract
 {
     public interface IAuthService
-    {
+    { 
         IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
         IDataResult<User> Login(UserForLoginDto userForLoginDto);
         IResult UserExists(string email);
